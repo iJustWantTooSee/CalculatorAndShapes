796edc7491809f541d83163a4f7e26fe19a0f241
Виктор Админ <viktor.skachkov.tomsk@mail.ru>
2021-01-19 12:04:47 +0700

    вторая часть

--- DOM/Frontend7/wwwroot/index.js
+++ DOM/Frontend7/wwwroot/index.js
@@ -1,3 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const list = document.querySelector('.list');
+$(window).load(function () {
+    const list = $('.list');
+    const firstElement = $('.item');
+    const result = $('.result');
+    const tempLine = firstElement.clone();
+    //console.log(list.children().first().children().eq(2));
+
+    list.children().first().children().eq(2).on('click', RaiseElement);
+    list.children().first().children().eq(3).on('click', DropElement);
+    list.children().first().children().eq(4).on('click', DeleteElement);
+    $('.add-button').on('click', CreateElement);
+    $('.save-button').on('click', SaveElement);
+
+    function RaiseElement(event) {
+        console.log(list.children().first());
+        console.log($(event.target).parent().prev());
+        if (list.children().first() != $(event.target).parent()) {
+            $(event.target).parent().after($(event.target).parent().prev());
+        }
+    }
+
+    function DropElement(event) {
+        if (list.children().last() != $(event.target).parent()) {
+            // event.target.parentNode.insertBefore(event.target.parentNode, event.target.parentNode.nextElementSibling);
+            $(event.target).parent().next().after($(event.target).parent());
+        }
+    }
+
+    function DeleteElement(event) {
+        $(event.target).parent().remove();
+    }
+
+    function CreateElement() {
+        var newElement = tempLine.clone();
+        list.append(newElement);
+
+        newElement.children().eq(2).on('click', RaiseElement);
+        newElement.children().eq(3).on('click', DropElement);
+        newElement.children().eq(4).on('click', DeleteElement);
+    }
+
+    function SaveElement() {
+        // result.innerHTML = JSON.stringify({ list })
+        //console.log(firstElement);
+        //console.log(firstElement.children[0].value);
+      
+        let listChildren = list.children();
+        let input = {};
+        
+        for (var i = 0; i < listChildren.length; i++) {
+            console.log(listChildren.eq(i).children().eq(0).val()); 
+            input[listChildren.eq(i).children().eq(0).val()] = listChildren.eq(i).children().eq(1).val();
+        }
+        result.html(JSON.stringify(input));
+    }
 });
